903ad770819966e1e5418ca0f425fc51b5bbea68
Will Cruse <willcruse123@gmail.com>
2018-11-17 16:41:36 +0000

    lies

--- getData.py
+++ getData.py
@@ -1,33 +1,32 @@
 import requests
 
-#ALL RETURNED AS JSON
 
-#returns JSON list of instruments
+#returns instruments JSON
 def getIntruments():
     request = requests.get("http://egchallenge.tech/instruments").json()
     return request
 
-#returns current epoch
+#returns current epoch INT
 def getCurrentEpoch():
     request = requests.get("http://egchallenge.tech/epoch").json()
     return request['current_epoch']
 
-#returns prediction epoch
+#returns prediction epoch INT
 def getPredictionEpoch():
     request = requests.get("http://egchallenge.tech/epoch").json()
     return request['prediction_epoch']
 
-#returns market data for specific instrument
+#returns market data for specific instrument JSON
 def getMarketDataIntrument(instrument):
     url = "http://egchallenge.tech/marketdata/instrument/" + instrument
     return requests.get(url).json()
 
-#returns market data for given epoch
+#returns market data for given epoch JSON
 def getMarketDataEpoch(epoch):
     url = "http://egchallenge.tech/marketdata/epoch/" + epoch
     return requests.get(url).json()
 
-#returns latest market data
+#returns latest market data JSON
 def getMarketDataLatest():
     return requests.get("http://egchallenge.tech/marketdata/latest").json()
 
