79440298ed8d9fa8f8c3944915fb2a51f5819922
Felix <felixmasters15@gmail.com>
2018-11-17 18:27:22 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ saveData.py
@@ -0,0 +1,21 @@
+#import pandas
+import getData
+import json
+
+def saveData():
+    with open('JsonStore.json', 'w') as f:
+        json.dump([], f)
+
+    data = []
+    for i in range(baseEpoch, getData.getCurrentEpoch()+1):
+        currentEpochDict = getData.getMarketDataEpoch(str(i))
+        data.append = currentEpochDict
+
+    with open('JsonStore.json', 'w') as f:
+        json.dump(data, f)
+
+
+baseEpoch = 3000
+
+saveData()
+
